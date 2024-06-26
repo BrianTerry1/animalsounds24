f525e37a8c70913e0765d4053d042eb1d8a52282
BrianTerry1 <56925848+BrianTerry1@users.noreply.github.com>
2024-06-26 13:26:23 +0100

    documentation

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,3 +1,16 @@
+#' Say what animal makes what sound
+#'
+#' A function which takes two character strings
+#' and says what animal makes what sound.
+#' Check out [paste0 here][base::paste0()]!
+#'
+#' @param animal A character string.
+#' @param sound A character string.
+#'
+#' @return A character string saying what animal makes what sound.
+#' @export
+#'
+#' @examples
 animal_sounds <- function(animal, sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
